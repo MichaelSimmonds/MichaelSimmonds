8a59b7ac260a0b75b2094c33712c2ba09eec2a18
MichaelSimmonds <mrmichaelsimmonds@gmail.com>
2017-03-21 16:29:55 +0100

    rails first#

---
+++ black_jack front/hand_obj.rb
@@ -0,0 +1,18 @@
+ideal object for front end
+
+- card1 - card5 in 1H - KH format
+total value of cards
+if hit, stand, bet, split is available
+
+current_hand = {
+  card1: "2H",
+  card2: "4S",
+  card3: "8C",
+  card4: "5S",
+  hand_total: 19,
+  hand_bet: 55,
+  hit: true,
+  stand: true,
+  bet: true,
+  split: false
+}
